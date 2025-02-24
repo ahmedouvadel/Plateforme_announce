db642e261a002f4e46b1c4fa62db3c81ca70624f
ahmedouvadel <100805939+ahmedouvadel@users.noreply.github.com>
2025-02-24 14:21:05 +0000

    add navbar

--- palteforme_announce/palteforme_announce/settings.py
+++ palteforme_announce/palteforme_announce/settings.py
@@ -55,11 +55,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "palteforme_announce.urls"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Ajout du dossier templates
+        'DIRS': [os.path.join(BASE_DIR,'theme', 'templates')],  # Ajout du dossier templates
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
